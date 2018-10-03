98d0b6a8aab04ba846fdd4d3caa90cb41e60e77d
Pedro Cabral <prcabral@users.noreply.github.com>
2018-10-03 10:15:52 +0200

    Update Assignment1.asm

--- Assignment1/Assignment1.asm
+++ Assignment1/Assignment1.asm
@@ -1,10 +1,10 @@
-; Title		:
-; Date		:
-; Author	: Pedro Cabral
-; Blog Post	:
-; Twitter	: https://twitter.com/CabrallPedro
+; Title		  : TCP bind shell
+; Date		  : 01/10/18
+; Author	  : Pedro Cabral
+; Blog Post	: https://binaryexploit.com/TCP_Bind_Shell_Shellcode/
+; Twitter	  : https://twitter.com/CabrallPedro
 ; LinkedIn	: https://www.linkedin.com/in/pedro-cabral1992
-; SLAE ID	: SLAE-1372
+; SLAE ID	  : SLAE-1372
 ; Tested on	: i686 GNU/Linux
 
 global _start
